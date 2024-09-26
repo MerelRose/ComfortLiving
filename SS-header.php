303e297ab99f54aea06d4ff7fe9705242aeaa70f
Merel Rose <mrosedevries@gmail.com>
2024-09-26 08:11:20 +0200

    header

---
+++ SS-header.php
@@ -0,0 +1,13 @@
+<html>
+    <link rel="stylesheet" href="styles/SS-header.css">
+    <header>
+        <div class="header-bg">
+            <img src="img/logo.png" alt="logo" width="10%" height="auto" class="logo">
+            <div class="button-group">
+                <button class="nav-btn">Overview</button>
+                <button class="nav-btn">About</button>
+                <button class="nav-btn">Login</button>
+            </div>
+        </div>
+    </header>
+</html>
