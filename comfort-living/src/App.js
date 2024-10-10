cd1b8fec81644d9238f6b7392e7fdd94877be980
Merel Rose <mrosedevries@gmail.com>
2024-10-10 09:02:06 +0200

    search

--- comfort-living/src/App.js
+++ comfort-living/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import logo from './logo.png';
 import './App.css';
 import Login from './Login';
+import Search from './search';
 import MyAccount from './MyAccount';
 import WarningPopup from './WarningPopup';
 
@@ -11,11 +12,11 @@ function App() {
     <BrowserRouter>
       <header>
         <div className="header-bg">
-          <img src={logo} className="logo" alt="logo" width="10%" height="auto"/>
-          <span className="slogan">Waar uw droomhuis werkelijkheid wordt</span>
+          <img src={logo} className="logo" alt="logo"/>
+          <span className="slogan">Waar uw droomhuis werkelijkheid wordt!</span>
           <div className="button-group">
-            <input type="text" className="nav-btn" placeholder="Search.."/>
-            <button className="nav-btn">Search</button>
+            {/* <input type="text" className="nav-btn" placeholder="Search.."/> */}
+            <button className="nav-btn" onClick={() => window.location.href = '/search'}>Search</button>
             <button className="nav-btn" onClick={() => window.location.href = '/login'}>Login</button>
             <button className="nav-btn" onClick={() => window.location.href = '/my-account'}>My Account</button>
           </div>
@@ -28,6 +29,7 @@ function App() {
       </div>
 
       <Routes className="content">
+        <Route path="/search" element={<Search />} />
         <Route path="/login" element={<Login />} />
         <Route path="/my-account" element={<MyAccount />} />
       </Routes>
