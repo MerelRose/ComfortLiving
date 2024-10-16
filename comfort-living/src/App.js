0d965c607b4fe8a27bcf4c822846d6732b85dd8e
JordytheCodemaster <124360795+JordytheCodemaster@users.noreply.github.com>
2024-10-16 11:14:48 +0200

    added detail page for houses

--- comfort-living/src/App.js
+++ comfort-living/src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import logo from './logo.png';
+import WoningDetail from './DetailPage';
 import './App.css';
 import LoginForm from './Login'; 
 import Register from './Register';
@@ -26,6 +27,7 @@ function App() {
           <span className="slogan">Waar uw droomhuis werkelijkheid wordt!</span>
           <div className="button-group">
             {/* <input type="text" className="nav-btn" placeholder="Search.." /> */}
+            
             <button className="nav-btn" onClick={() => window.location.href = '/search'}>Search</button>
             <button className="nav-btn" onClick={() => setIsLoginOpen(true)}>Login</button>
             <button className="nav-btn" onClick={() => setIsRegisterOpen(true)}>Register</button>
@@ -43,6 +45,9 @@ function App() {
         <Route path="/login" element={<LoginForm />} />
         <Route path="/my-account" element={<MyAccount />} />
         <Route path="/login" element={<div />} />
+        <Route path="/woning/:id" element={<WoningDetail />} />
+
+        <Route path="/detailpage" element={<WoningDetail />} />
         <Route path="/register" element={<div />} />
       </Routes>
       </div>
