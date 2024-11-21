dd7fd82c616424dbdbdebcdac75775f0cc83a58c
Merel Rose <123648592+MerelRose@users.noreply.github.com>
2024-11-21 10:09:12 +0100

    detailpage map

--- comfort-living/src/DetailPage.js
+++ comfort-living/src/DetailPage.js
@@ -1,16 +1,34 @@
 import React, { useEffect, useState, useContext } from 'react';
 import { useParams } from 'react-router-dom';
 import { AuthContext } from './AuthContext';
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 import './App.css';
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// Fix Leaflet marker icon issue
+L.Marker.prototype.options.icon = L.icon({
+    iconUrl: markerIcon,
+    iconRetinaUrl: markerIcon2x,
+    shadowUrl: markerShadow,
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
+});
 
 const WoningDetail = () => {
     const { id } = useParams();
     const [woning, setWoning] = useState(null);
     const [loading, setLoading] = useState(true);
-    const { isLoggedIn, user } = useContext(AuthContext); 
+    const { isLoggedIn, user } = useContext(AuthContext);
     const [isOpen, setIsOpen] = useState(false);
 
-    // State voor het inschrijfformulier
+    // State for the subscription form
     const [showInschrijfForm, setShowInschrijfForm] = useState(false);
     const [hoeveel_personen, setHoeveelPersonen] = useState('');
     const [jaar_inkomen, setJaarInkomen] = useState('');
@@ -21,9 +39,10 @@ const WoningDetail = () => {
             try {
                 const response = await fetch(`http://localhost:3001/panden/${id}`, {             
                     headers: {
-                    "api-key": 'AIzaSyD-1uJ2J3QeQK9nKQJ9v6ZJ1Jzv6J1Jzv6',
-                    "Content-Type": "application/json"
-                }});
+                        "api-key": 'AIzaSyD-1uJ2J3QeQK9nKQJ9v6ZJ1Jzv6J1Jzv6',
+                        "Content-Type": "application/json"
+                    }
+                });
                 if (!response.ok) throw new Error('Woning niet gevonden');
                 const data = await response.json();
                 console.log('Woningdata succesvol opgehaald:', data);
@@ -45,7 +64,6 @@ const WoningDetail = () => {
         console.log('Aantal personen:', hoeveel_personen);
         console.log('Jaar inkomen:', jaar_inkomen);
     
-        // Controleer of de gebruiker is ingelogd voordat je de fetch uitvoert
         if (!isLoggedIn) {
             setInschrijfMessage('U moet eerst inloggen om u in te kunnen schrijven.');
             console.log('Inschrijving mislukt: gebruiker niet ingelogd');
@@ -68,8 +86,6 @@ const WoningDetail = () => {
             });
     
             const data = await response.json();
-    
-            // Controleer of de inschrijving succesvol was
             if (response.ok) {
                 console.log('Inschrijving succesvol:', data);
                 setInschrijfMessage('Inschrijving succesvol!');
@@ -85,19 +101,17 @@ const WoningDetail = () => {
             setInschrijfMessage('Er is een fout opgetreden bij het inschrijven.');
         }
     };
-    
+
     const handleOpenPopup = () => {
         console.log('Pop-up geopend');
         setIsOpen(true);
     };
-    
 
     const handleClosePopup = (e) => {
         e.preventDefault();
         console.log('Pop-up gesloten');
         setIsOpen(false);
     };
-    
 
     if (loading) {
         return <div>Loading...</div>;
@@ -106,6 +120,8 @@ const WoningDetail = () => {
     if (!woning) {
         return <div>Woning niet gevonden</div>;
     }
+
+    const { latitude, longitude } = woning;
 
     return (
         <div className='content'>
@@ -116,9 +132,10 @@ const WoningDetail = () => {
                 <p>Type: {woning.type}</p>
                 <p>Omschrijving: {woning.omschrijving}</p>
                 <p>Prijs: {woning.prijs}</p>
-                <p>energielabel: {woning.prijs}</p>
-                <p>aantal slaapkamers: {woning.prijs}</p>
-                <p>Prijs: {woning.prijs}</p>
+                <p>energielabel: {woning.energielabel}</p>
+                <p>aantal slaapkamers: {woning.slaapkamers}</p>
+                <p>Latitude: {latitude}</p>
+                <p>Longitude: {longitude}</p>
 
                 <button className='nav-btn' onClick={handleOpenPopup}>Open Pop-up</button>
 
@@ -150,6 +167,25 @@ const WoningDetail = () => {
                         </div>
                     </div>
                 )}
+            </div>
+
+            <div style={{ height: "400px", width: "100%" }}>
+                <MapContainer
+                    center={[latitude, longitude]}
+                    zoom={13}
+                    style={{ height: "100%", width: "100%" }}
+                >
+                    <TileLayer
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    />
+                    <Marker position={[latitude, longitude]}>
+                        <Popup>
+                            Latitude: {latitude} <br />
+                            Longitude: {longitude}
+                        </Popup>
+                    </Marker>
+                </MapContainer>
             </div>
         </div>
     );
